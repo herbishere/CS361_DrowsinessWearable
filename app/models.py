d4aa8405b25caf39a7e64129651efe290faa168f
Herb <herbishere@users.noreply.github.com>
2019-11-25 20:53:04 -0600

    implemented additional databases + initialization

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 
+
 class Driver(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     device = db.Column(db.String(128))
@@ -7,9 +8,10 @@ class Driver(db.Model):
     version = db.Column(db.String(128))
     releaseDate = db.Column(db.String(128))
     mostCurrent = db.Column(db.String(128))
-    
+
     def __repr__(self):
         return '<Driver: {}; Version: {}>'.format(self.driverName, self.version)
+
 
 class WearableInfo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,4 +22,16 @@ class WearableInfo(db.Model):
 
     def __repr__(self):
         return '<Wearable: {}; Type: {}>'.format(self.name, self.type)
-    
+
+
+class PhysData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(128))
+    time = db.Column(db.String(128))
+    heartrate = db.Column(db.Integer)
+    overalldrowsiness = db.Column(db.Integer)
+    alertstatus = db.Column(db.String(128))
+    timeelapsed = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<Date: {}; Drowsiness: {}; Alert Status: {}>'.format(self.date, self.overalldrowsiness, self.alertstatus)
